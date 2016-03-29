8cf66f12e2e8c849201146b92f6b512a66eaa93a
paflechien33 <thierry.masson@gmail.com>
2016-03-30 01:07:21 +0200

    my best f95 submission so far

---
+++ DIGITOFE_F/F_SRC/DIGITOFE.f95
@@ -0,0 +1,26 @@
+program TEST
+    integer :: n,m,i,j,k(18500)
+    character (len=4) :: zzformat
+    zzformat ='(I1)'
+    !real :: pr
+    m=0
+    k(1)=0
+    k(2)=2
+    !pr = 2.7
+    ! init tableau
+    do j=3,18500
+      k(j)=1
+    end do
+    write (*,'(A3)',advance='no') '2.7'
+    !calc
+    do n=18499,9,-1
+      do i=n+1,2,-1
+        k(i) = mod(m,i)
+        m = 10*k((i-1)) + m/i
+      end do
+      if(n<18499) then
+      write (*,zzformat,advance='no') modulo(m,10)
+      end if
+    end do
+    stop
+end program TEST
